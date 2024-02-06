a07ae90181b0d2013446410f86bf45736f6dd8a7
christiansegura <segurac@pdx.odshp.com>
2024-02-06 07:55:14 -0800

    re-familiarizing with require and module.exports

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const getNotes = require('./notes.js');
 const fs = require('fs');
 
 // fs.writeFileSync('notes.txt', 'This file was created by Node.js. ');
 fs.appendFileSync('notes.txt',' This is some new text I added with a different method.');
+
+console.log(getNotes());
 
