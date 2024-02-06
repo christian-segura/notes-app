5b2c8cef0ad0feffad5f60c4aece6c0554b9ff24
christiansegura <segurac@pdx.odshp.com>
2024-02-06 07:18:33 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'This file was created by Node.js. ');
+fs.appendFileSync('notes.txt',' This is some new text I added with a different method.');
+
